fd1b415cc3b8aa58536c02bedeba5d028c552d68
月生子 <9569006+monthly-child@user.noreply.gitee.com>
2021-12-10 17:36:20 +0800

    完成了报表功能

---
+++ src/components/report/report.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="report_container">
+    <!-- 用户区域面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片区域 -->
+    <el-card>
+      <div id="main" style="width: 1000px; height: 400px"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+import _ from 'lodash'
+export default {
+  data() {
+    return {
+      option: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    }
+  },
+  async mounted() {
+    var myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) return this.$message.error('获取折线图数据失败')
+    const result = _.merge(res.data, this.option)
+    myChart.setOption(result)
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '@/components/goods/params.vue'
 import Goods from '@/components/goods/Goods.vue'
 import Add from '@/components/goods/Add.vue'
 import Order from '@/components/orders/order.vue'
+import report from '@/components/report/report.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -29,7 +30,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: Goods },
       { path: '/goods/add', component: Add },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: report }
     ]
   }
 ]
